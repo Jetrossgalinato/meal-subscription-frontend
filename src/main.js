daaa16cb21578c0ad076d0a76c7456054f614a58
Jetross <jetrossneri07@gmail.com>
2025-03-26 18:52:22 +0800

    imported vue-toastification to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import { createVuetify } from "vuetify";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@mdi/font/css/materialdesignicons.css";
+import "vue-toastification/dist/index.css";
+import { Toast } from "vue-toastification";
 import "./style.css";
 import "vuetify/styles";
 import App from "./App.vue";
