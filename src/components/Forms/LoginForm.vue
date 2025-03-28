388f19a16fdba768ea64d45084088084f409aa74
Jetross <jetrossneri07@gmail.com>
2025-03-28 20:17:32 +0800

    centered the card of the login form

--- src/components/Forms/LoginForm.vue
+++ src/components/Forms/LoginForm.vue
@@ -1,27 +1,15 @@
 <template>
   <v-container class="d-flex justify-center align-center" style="height: 100vh">
-    <v-card class="pa-6" max-width="400">
-      <v-card-title class="text-h5">Login</v-card-title>
-      <v-card-text>
-        <v-form @submit.prevent="handleLogin">
-          <v-text-field
-            v-model="email"
-            label="Email"
-            type="email"
-            required
-            outlined
-          ></v-text-field>
-          <v-text-field
-            v-model="password"
-            label="Password"
-            type="password"
-            required
-            outlined
-          ></v-text-field>
-          <v-btn type="submit" color="primary" block class="mt-4">
-            Login
-          </v-btn>
-        </v-form>
+    <v-card class="pa-5" width="400">
+      <template v-slot:title>
+        <span class="font-weight-black">Welcome to Vuetify</span>
+      </template>
+
+      <v-card-text class="bg-surface-light pt-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+        ratione debitis quis est labore voluptatibus! Eaque cupiditate minima,
+        at placeat totam, magni doloremque veniam neque porro libero rerum unde
+        voluptatem!
       </v-card-text>
     </v-card>
   </v-container>
