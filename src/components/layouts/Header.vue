f1454fbb964870a137c503d4b8eca9fffa9e5cf3
Jetross <jetrossneri07@gmail.com>
2025-03-28 18:12:42 +0800

    added margin on icon and title of navbar

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app-bar :elevation="2">
     <template v-slot:prepend>
-      <v-icon>mdi-format-list-bulleted-square</v-icon>
+      <v-icon class="ml-6">mdi-format-list-bulleted-square</v-icon>
     </template>
 
-    <v-app-bar-title>Application Bar</v-app-bar-title>
+    <v-app-bar-title class="ml-4">Application Bar</v-app-bar-title>
   </v-app-bar>
 </template>
 
