3cef7a8eb68cdde1bd66915aa17c585421f63e0e
Jetross <jetrossneri07@gmail.com>
2025-03-28 18:37:23 +0800

    changed the name of the system

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app-bar :elevation="2">
-    <v-app-bar-title class="ml-8">Application Bar</v-app-bar-title>
+  <v-app-bar :elevation="4">
+    <v-app-bar-title class="ml-8">MealMate</v-app-bar-title>
   </v-app-bar>
 </template>
 
