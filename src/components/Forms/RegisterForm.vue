1698729ed0200cd82ec3d7e66a5bf46f8df67126
Jetross <jetrossneri07@gmail.com>
2025-04-01 22:53:43 +0800

    added background on registration form

--- src/components/Forms/RegisterForm.vue
+++ src/components/Forms/RegisterForm.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-container class="d-flex justify-center align-center" style="height: 100vh">
+  <v-card
+    class="d-flex justify-center align-center login-background"
+    style="height: 100vh"
+  >
     <v-card class="pa-5 elevation-8" width="400">
       <template v-slot:title>
         <div class="text-center">
@@ -35,14 +38,14 @@
           <v-btn class="mt-2" type="submit" block>Register</v-btn>
         </v-form>
         <div class="text-center mt-2">
-          <span
-            >Already have an account?
-            <router-link to="/">Login</router-link></span
-          >
+          <span>
+            Already have an account?
+            <router-link to="/">Login</router-link>
+          </span>
         </div>
       </v-sheet>
     </v-card>
-  </v-container>
+  </v-card>
 </template>
 
 <script setup>
@@ -92,5 +95,21 @@ const handleRegister = async () => {
 </script>
 
 <style scoped>
-/* Add custom styles if needed */
+/* Add a background image to the registration page */
+.login-background {
+  background-image: url("https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?t=st=1743517535~exp=1743521135~hmac=81b153c993a81dfba0107f7fb1da252febcbc788a57200339af7c5f8da6dd715&w=1380");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 100%;
+}
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden; /* Prevent scrolling */
+}
 </style>
